36b61fbbb74b41fa8f693bc8913d58bb2dd34e79
ChauDev Software <158478450+chaudevsoftware@users.noreply.github.com>
2024-05-30 23:18:42 +0700

    Create calc.py

---
+++ calc.py
@@ -0,0 +1,14 @@
+# https://github.com/chaudevsoftware/pycalc/ 
+# You can delete the links and this line too!
+
+# This gonna find user number to calculate!
+num1 = input("First Number: ")
+num2 = input("Second Number: ")
+
+# Result:
+result = float(num1) + float(num2)
+# Float in python is a Decimals point!. Number [1] --> 1.0
+
+print(str(result) + " Done Calculate!")
+# Basicly is gonna print Result and then say Done Calculate!
+# str is a String for add an Text like that!
